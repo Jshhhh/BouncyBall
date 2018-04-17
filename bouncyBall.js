45e49e3f01aa645246c28798d0a4f55ab3559401
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2018-04-17 10:23:00 -0700

    Added pseudocode for level 1 function

--- bouncyBall.js
+++ bouncyBall.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+	//on document ready
+		//ball starts bounce at level 1
+	//run level one bounce function once;
+		//set css y axis ball to increment after certain setTimeout interval < 500ms
+		//when y increments to ~500 px then start decrement to ~100
+		//increment back to 500
+
+
+	//on ball click
+		//ball disappears
+		//display congratulatory message and "next level: 2"
+		//for 3000 ms
+		//then begin next level
+		//up to 10, then ask if user wants to start again(refresh page or start level 1)
 
 });
